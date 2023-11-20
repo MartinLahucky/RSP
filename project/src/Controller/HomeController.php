f5c550418c77e743101ee38314cb0f3f19bcb473
FoxDeFacto <lisakzezhore@seznam.cz>
2023-11-20 21:40:21 +0100

    Poznámky pro další vývoj xD

--- project/src/Controller/HomeController.php
+++ project/src/Controller/HomeController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Clanek;
+use App\Entity\RecenzniRizeni;
+use App\Entity\Role;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
-use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class HomeController extends AbstractController
 {
@@ -30,5 +36,44 @@ class HomeController extends AbstractController
         $mailer->send($email);
 
         return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/createClanek', name: 'app_create_clanek')] //Dodělat xD OmehaLuL
+    public function createClanek(Request $request, ManagerRegistry $doctrine):Response
+    {
+        if ($this->getUser()==null) 
+        {
+            throw $this->createAccessDeniedException("lol nemáš práva xD");
+        }
+
+        if (!in_array(Role::ADMIN->value, $this->getUser()->getRoles())) 
+        {
+            throw $this->createAccessDeniedException("lol nemáš práva xD");
+        }
+
+        $clanek = new Clanek(); 
+        //Každý článěk bude v intervalu pro určité recenzní řízení, které bude představovat množinu, do které se budou řadit články, schválené články se zařadí do tisku
+        //Prvně se vytvoří tisk o určité kapacitě, pro daný tisk se vytvoří rezenzní řízení do kterého se budou řadit články vytvořené a schálené v daném intervalu rezenzního řízení a následně se to přesune do tisku
+        $form = $this->createForm(ClanekFormType::class, $clanek);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $clanek = $form->getData();
+
+            $em = $doctrine->getManager();
+
+            $em->persist($clanek);
+
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        // Render the form view in your template
+        return $this->render('home/create-clanek.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
